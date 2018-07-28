c8709ee59c50b3c71e356a402a591c35313b472b
Travis Sansome <travis.sansome@empired.com>
2018-07-28 17:26:38 +1000

    Fixed bug where if template called from outside the project directory. Code wasn't using relative paths and would hit error. Moved generated folder to having to be specified as command line arg. Fixed some issues in the DataLake Store Databricks scripts R2M.

--- template.js
+++ template.js
@@ -9,20 +9,23 @@ var program = require('commander');
  
 program
   .version('0.1.0')
-  .option('-c, --config [path]', 'Path to config file to use')
+  .option('-c, --config <filepath>', 'File Path to config file to use.')
+  .option('-g, --generated <folderpath>', 'Folder Path to location to put generated folders.')
+  .option('-s, --samples [folderpath]', 'Folder Path to location of samples. (Optional)')
   .parse(process.argv);
 
 
 var configPath = path.resolve(program.config)
-var obj = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-var generatedFolder = path.dirname(configPath) + "/generated/";
+var templatorConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+var generatedFolder = path.resolve(program.generated);
 if (!fs.existsSync(generatedFolder)) fs.mkdirSync(generatedFolder);
 
 var outputtedFilesCount = 0;
-console.log("Found " + obj.datasets.length + " datasets to generate code for.");
-for(var dataset_index in obj.datasets) {
+console.log("Found " + templatorConfig.datasets.length + " datasets to generate code for.");
+//di = dataset index
+for(var di in templatorConfig.datasets) {
     //now that we have the dataset
-    dataset_to_generate = obj.datasets[dataset_index];
+    dataset_to_generate = templatorConfig.datasets[di];
     //let's just apply mustache on the config so they can use anything in the dataset
     var template = JSON.stringify(dataset_to_generate);
     var new_dataset_to_generate_string = Mustache.render(template, dataset_to_generate);
@@ -31,55 +34,59 @@ for(var dataset_index in obj.datasets) {
     //#TODO: Validation of config
     //now let's loop through the patterns requested
     console.log(dataset_to_generate.patterns.length + " patterns requested for generation.");
+    //pi = index of the pattern
     for(var pi in dataset_to_generate.patterns) {
         var pattern_to_generate = dataset_to_generate.patterns[pi];
         //ensure that the pattern requested is supported
-        assert.strictEqual(fs.existsSync("templates/" + pattern_to_generate.name.toUpperCase() + "/"), true, "The pattern " + pattern_to_generate.name.toUpperCase() + " is not one of the supported patterns. Please use one of the following: blah");
+        var pattern_folder_path = path.resolve(__dirname + "/templates/" + pattern_to_generate.name.toUpperCase() + "/");
+        assert.strictEqual(fs.existsSync(pattern_folder_path), true, "The pattern " + pattern_to_generate.name.toUpperCase() + " is not one of the supported patterns. Please use one of the following: blah");
         //now loop through the languages requested
-        for (var pattern_language_implementation_index in pattern_to_generate.generate) {
-            var pattern_language_implementation_config = pattern_to_generate.generate[pattern_language_implementation_index];
+        //pli = index of the language to implement for the pattern
+        for (var pli in pattern_to_generate.generate) {
+            var pattern_language_implementation_config = pattern_to_generate.generate[pli];
             //now prepare the final config for the mustache template
-            var dsFinalConfig = dataset_to_generate;
+            var dataSetFinalConfig = dataset_to_generate;
             if (dataset_to_generate.source == null) dataset_to_generate.source = {};
             if (pattern_language_implementation_config.source == null) pattern_language_implementation_config.source = {};
             if (dataset_to_generate.target == null) dataset_to_generate.target = {};
             if (pattern_language_implementation_config.target == null) pattern_language_implementation_config.target = {};
-            dsFinalConfig.source = Object.assign(dataset_to_generate.source, pattern_language_implementation_config.source);
-            dsFinalConfig.target = Object.assign(dataset_to_generate.target, pattern_language_implementation_config.target);
-            dsFinalConfig.language = pattern_language_implementation_config.language;
-            dsFinalConfig.pattern = pattern_to_generate.name;
+            dataSetFinalConfig.source = Object.assign(dataset_to_generate.source, pattern_language_implementation_config.source);
+            dataSetFinalConfig.target = Object.assign(dataset_to_generate.target, pattern_language_implementation_config.target);
+            dataSetFinalConfig.language = pattern_language_implementation_config.language;
+            dataSetFinalConfig.pattern = pattern_to_generate.name;
             //now for the arrays like columns and date_columns we need to add a last boolean to help with string generation
-            if (dsFinalConfig.source.columns == null) dsFinalConfig.source.columns = [];
-            if (dsFinalConfig.source.date_columns == null) dsFinalConfig.source.date_columns = [];
-            if (dsFinalConfig.source.primary_key == null) dsFinalConfig.source.primary_key = [];
-            dsFinalConfig.source.columns[dsFinalConfig.source.columns.length - 1].last = true;
-            dsFinalConfig.source.date_columns[dsFinalConfig.source.date_columns.length - 1].last = true;
-            dsFinalConfig.source.primary_key[dsFinalConfig.source.primary_key.length - 1].last = true;
+            if (dataSetFinalConfig.source.columns == null) dataSetFinalConfig.source.columns = [];
+            if (dataSetFinalConfig.source.date_columns == null) dataSetFinalConfig.source.date_columns = [];
+            if (dataSetFinalConfig.source.primary_key == null) dataSetFinalConfig.source.primary_key = [];
+            dataSetFinalConfig.source.columns[dataSetFinalConfig.source.columns.length - 1].last = true;
+            dataSetFinalConfig.source.date_columns[dataSetFinalConfig.source.date_columns.length - 1].last = true;
+            dataSetFinalConfig.source.primary_key[dataSetFinalConfig.source.primary_key.length - 1].last = true;
             //now let's render
             //we need to choose the right template
             //find the template folder
-            var folderPath = "templates/" + dsFinalConfig.pattern.toUpperCase() + "/" + dsFinalConfig.language.toUpperCase() + "/";
-            //ensure that the language requested is supported and has a scripts_config
-            assert.strictEqual(fs.existsSync("templates/" + pattern_to_generate.name.toUpperCase() + "/" + pattern_language_implementation_config.language.toUpperCase()), true, "The pattern " + pattern_to_generate.name.toUpperCase() + " does not support " + pattern_language_implementation_config.language.toUpperCase() + " as language, please use one of the following: blah ...");
+            var languageFolderPath = pattern_folder_path + "/" + pattern_language_implementation_config.language.toUpperCase();
+            //ensure that the language requested is supported
+            assert.strictEqual(fs.existsSync(languageFolderPath), true, "The pattern " + pattern_to_generate.name.toUpperCase() + " does not support " + pattern_language_implementation_config.language.toUpperCase() + " as language, please use one of the following: blah ...");
             //get the template files under that language
-            var templateFiles = fs.readdirSync(folderPath).filter(function(file) { return file.substr(-9) == ".mustache" });
+            var templateFiles = fs.readdirSync(languageFolderPath).filter(function(file) { return file.substr(-9) == ".mustache" });
             //read in the scripts config file for the template files
-            assert.strictEqual(fs.existsSync(folderPath + "scripts_config.json"), true, "A scripts config file is not present for the language " + pattern_language_implementation_config.language.toUpperCase() + " for the pattern " + pattern_to_generate.name.toUpperCase());
-            var script_configs = JSON.parse(fs.readFileSync(folderPath + "scripts_config.json", 'utf8'));
+            var scriptsConfigPath = languageFolderPath + "/scripts_config.json"
+            assert.strictEqual(fs.existsSync(scriptsConfigPath), true, "A scripts config file is not present for the language " + pattern_language_implementation_config.language.toUpperCase() + " for the pattern " + pattern_to_generate.name.toUpperCase());
+            var scriptConfigs = JSON.parse(fs.readFileSync(scriptsConfigPath, 'utf8'));
             //now loop and generate
             for (var fi in templateFiles) {
-                var fp = folderPath + templateFiles[fi];
+                var fp = languageFolderPath + "/" + templateFiles[fi];
                 //look for a valid config
-                var scriptConf = script_configs.filter(function(conf) { return conf.script_name + ".mustache" == templateFiles[fi] });
-                assert.strictEqual(scriptConf.length, 1, "1 config should be defined for " + templateFiles[fi] + " in the folder " + folderPath);                        
+                var scriptConf = scriptConfigs.filter(function(conf) { return conf.script_name + ".mustache" == templateFiles[fi] });
+                assert.strictEqual(scriptConf.length, 1, "1 config should be defined for " + templateFiles[fi] + " in the folder " + languageFolderPath);                        
                 //so template it
                 var template_str = fs.readFileSync(fp, 'utf8');
-                var output = Mustache.render(template_str, dsFinalConfig);                        
+                var output = Mustache.render(template_str, dataSetFinalConfig);                        
                 //prepare the output folder if not present
-                var output_filedir = generatedFolder + dsFinalConfig.language; 
+                var output_filedir = generatedFolder + dataSetFinalConfig.language; 
                 if (!fs.existsSync(output_filedir)) fs.mkdirSync(output_filedir);
                 //write out the file
-                var outputFileNameWithExt = Mustache.render(scriptConf[0].output.filename, dsFinalConfig) + "." + scriptConf[0].output.extension;                        
+                var outputFileNameWithExt = Mustache.render(scriptConf[0].output.filename, dataSetFinalConfig) + "." + scriptConf[0].output.extension;                        
                 fs.writeFileSync(output_filedir + "/" + outputFileNameWithExt, output);
                 outputtedFilesCount++;
             }
