20dcfdecedb3074864154891e0cf32f23274fb86
Travis Sansome <travis.sansome@empired.com>
2018-08-23 13:07:07 +1000

    Implemented 4 of the test cases

--- tests/config.test.js
+++ tests/config.test.js
@@ -1,19 +1,181 @@
 //Mustaching content inside config
+const templator = require('../src/templater-core.js');
 
 test('Able to refer to a source variable in a config from parent', () => {
-    expect(1+2).toBe(3);
+    
+    var config = {
+        "name": "MyTable",
+        "source": {
+            "database": "SourceDB",
+            "schema": "SourceSchema",
+            "tablename": "SourceTableName",                
+            "primary_key": [ { "name": "PersonID" }],
+            "columns": [{ "name": "PersonID" },
+                        { "name": "FirstName" },
+                        { "name": "LastName" }],
+            "date_columns": [ {"name": "Date_Created" }]
+        },
+        "templates": [
+            {
+                "name": "0_HELLOWORLD",
+                "generate": [
+                    {
+                        "language": "REL",
+                        "target": {
+                            "database": "TargetDB",
+                            "table": "{{source.database}}_{{ source.tablename}}",
+                            "date_format": "yyyy-MM-dd"
+                        }
+                    }
+                ]
+            }
+        ]
+    };    
+
+    var pattern_to_template = config.templates[0].generate[0];
+    
+    var final_config = templator.prepare_final_config(config, pattern_to_template, "DL1");
+
+    var template_definition = "{{source.tablename}}";
+    
+    var output = templator.generate_file_content_from_template(template_definition, final_config)
+
+    expect(output).toBe(config.source.tablename);
 });
 
 test('Able to refer to use a target variable in a config from parent', () => {
-    expect(1+2).toBe(3);
+    var config = {
+        "name": "MyTable",
+        "source": {
+            "database": "SourceDB",
+            "schema": "SourceSchema",
+            "tablename": "SourceTableName",                
+            "primary_key": [ { "name": "PersonID" }],
+            "columns": [{ "name": "PersonID" },
+                        { "name": "FirstName" },
+                        { "name": "LastName" }],
+            "date_columns": [ {"name": "Date_Created" }]
+        },
+        "target": {
+            "database": "TestDB"
+        },
+        "templates": [
+            {
+                "name": "0_HELLOWORLD",
+                "generate": [
+                    {
+                        "language": "REL",
+                        "source": {
+                            "service": "test"
+                        },
+                        "target": {
+                            "database": "TargetDB",
+                            "table": "{{source.database}}_{{ source.tablename}}",
+                            "date_format": "yyyy-MM-dd"
+                        }
+                    }
+                ]
+            }
+        ]
+    };    
+
+    var pattern_to_template = config.templates[0].generate[0];
+    
+    var final_config = templator.prepare_final_config(config, pattern_to_template, "DL1");
+
+    var template_definition = "{{target.database}}";
+    
+    var output = templator.generate_file_content_from_template(template_definition, final_config)
+
+    expect(output).toBe(config.target.database);
 });
 
 test('Able to refer to a source variable in a config from dataset', () => {
-    expect(1+2).toBe(3);
+    var config = {
+        "name": "MyTable",
+        "source": {
+            "database": "SourceDB",
+            "schema": "SourceSchema",
+            "tablename": "SourceTableName",                
+            "primary_key": [ { "name": "PersonID" }],
+            "columns": [{ "name": "PersonID" },
+                        { "name": "FirstName" },
+                        { "name": "LastName" }],
+            "date_columns": [ {"name": "Date_Created" }]
+        },
+        "templates": [
+            {
+                "name": "0_HELLOWORLD",
+                "generate": [
+                    {
+                        "language": "REL",
+                        "source": {
+                            "service": "test"
+                        },
+                        "target": {
+                            "database": "TargetDB",
+                            "table": "{{source.database}}_{{ source.tablename}}",
+                            "date_format": "yyyy-MM-dd"
+                        }
+                    }
+                ]
+            }
+        ]
+    };    
+
+    var pattern_to_template = config.templates[0].generate[0];
+    
+    var final_config = templator.prepare_final_config(config, pattern_to_template, "DL1");
+
+    var template_definition = "{{source.service}}";
+    
+    var output = templator.generate_file_content_from_template(template_definition, final_config)
+
+    expect(output).toBe(config.templates[0].generate[0].source.service);
 });
 
 test('Able to refer to a target variable in a config from dataset', () => {
-    expect(1+2).toBe(3);
+    var config = {
+        "name": "MyTable",
+        "source": {
+            "database": "SourceDB",
+            "schema": "SourceSchema",
+            "tablename": "SourceTableName",                
+            "primary_key": [ { "name": "PersonID" }],
+            "columns": [{ "name": "PersonID" },
+                        { "name": "FirstName" },
+                        { "name": "LastName" }],
+            "date_columns": [ {"name": "Date_Created" }]
+        },
+        "templates": [
+            {
+                "name": "0_HELLOWORLD",
+                "generate": [
+                    {
+                        "language": "REL",
+                        "source": {
+                            "service": "test"
+                        },
+                        "target": {
+                            "database": "TargetDB",
+                            "table": "{{source.database}}_{{ source.tablename}}",
+                            "date_format": "yyyy-MM-dd"
+                        }
+                    }
+                ]
+            }
+        ]
+    };    
+
+    var pattern_to_template = config.templates[0].generate[0];
+    
+    var final_config = templator.prepare_final_config(config, pattern_to_template, "DL1");
+
+    var template_definition = "{{target.database}}";
+    
+    var output = templator.generate_file_content_from_template(template_definition, final_config)
+
+    expect(output).toBe(config.templates[0].generate[0].target.database);
 });
 
 test('Able to refer to a user defined variable from parent in config', () => {
