1f6d0b06445a17f864427e06d63fc3f0b77726b3
tsansome <travis.sansome@gmail.com>
2018-10-15 13:35:28 +1100

    Moving into proper use of commander

--- template.js
+++ template.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var program = require('commander');
  
 const templater = require('./src/templater-core.js');
@@ -7,28 +9,31 @@ const assert = require("assert");
 
 program
   .version('0.1.0')
-  .option('-c, --config <filepath/folderpath>', 'File / Folder Path to a single or collection of config files to use.')
-  .option('-o, --outputs <folderpath>', 'Folder Path to location to put outputted folders / files.')
+  .command('template <config_path> <output_folder_path>')
+  .description("A program designed to take one or more config declarations and generate code to be published for automating data transformation / ingestion.")
   .option('-s, --samples [folderpath]', 'Folder Path to location of samples. (Optional)')
   .option('-l, --log [logLevel]', 'Log level to output (INFO, WARN, DEBUG, ERROR)')
-  .parse(process.argv);
+  .action(function(config_path, output_folder_path) {
 
-var configPath = path.resolve(program.config)
-var generatedFolder = path.resolve(program.outputs);
+    var configPath = path.resolve(config_path)
+    var generatedFolder = path.resolve(output_folder_path);
 
-if (fs.lstatSync(configPath).isDirectory()) {
-    //pointing at a folder of configs, configs should be in mustache format and have _config at the end of the filename
-    var configs = fs.readdirSync(configPath)
-                    .filter(function(fp) {
-                        return (path.extname(fp) == ".mustache" || path.extname(fp) == ".json") && path.basename(fp, path.extname(fp)).indexOf("_config") > -1
-                    });
-    console.log("Okay pointing at a folder of configs, found " + configs.length + " to process.");
-    for(var ci in configs) {
-        var configForProcessing = configs[ci];
-        var configForProcessingPath = configPath + "/" + configForProcessing;
-        templater.process_config(configForProcessingPath, generatedFolder, program.samples, program.logLevel);
+    if (fs.lstatSync(configPath).isDirectory()) {
+        //pointing at a folder of configs, configs should be in mustache format and have _config at the end of the filename
+        var configs = fs.readdirSync(configPath)
+                        .filter(function(fp) {
+                            return (path.extname(fp) == ".mustache" || path.extname(fp) == ".json") && path.basename(fp, path.extname(fp)).indexOf("_config") > -1
+                        });
+        console.log("Okay pointing at a folder of configs, found " + configs.length + " to process.");
+        for(var ci in configs) {
+            var configForProcessing = configs[ci];
+            var configForProcessingPath = configPath + "/" + configForProcessing;
+            templater.process_config(configForProcessingPath, generatedFolder, program.samples, program.logLevel);
+        }
+    } else {
+        //pointing at a specific config
+        templater.process_config(configPath, generatedFolder, program.samples, program.log);
     }
-} else {
-    //pointing at a specific config
-    templater.process_config(configPath, generatedFolder, program.samples, program.log);
-}
+  });
+
+program.parse(process.argv);
